556708994aed7aa2c8318290b40bb31d0ef3d476
aver-d <aver-d@someserver.com>
2014-11-09 15:30:21 +0000

    simple update

--- Asteroids.elm
+++ Asteroids.elm
@@ -3,10 +3,11 @@ import Vec2d (Vec)
 import Vec2d as V
 import Window
 import Rand
+import Debug
 
 fl = toFloat
 
-type Input = { delta:Time }
+type Input = { dt:Time }
 
 type Asteroid = {pos: Vec, vel: Vec, radius: Float, points: [Vec]}
 type Game = {state:State, asteroids:[Asteroid]}
@@ -14,13 +15,7 @@ data State = Play
 
 
 
--- Inputs
-delta = inSeconds <~ fps 30
-inputAll = sampleOn delta (Input <~ delta)
-
 -- Entities
-
-
 
 newAsteroid : Float -> Float -> Rand.Seed -> (Asteroid, Rand.Seed)
 newAsteroid x y seed =
@@ -37,6 +32,13 @@ newAsteroid x y seed =
     ({pos = (x,y), vel = (fl vx, fl vy), radius = radius, points = points}, seed5)
 
 
+stepAsteroid : Float -> Asteroid -> Asteroid
+stepAsteroid dt a =
+  { a | pos <- V.add a.pos (V.mul a.vel dt)}
+
+
+
+
 -- Render
 formAsteroid : Asteroid -> Form
 formAsteroid {pos, radius} = circle radius |> filled black |> move pos
@@ -46,9 +48,8 @@ render : (Int,Int) -> Game -> Element
 render (w, h) ({state, asteroids} as game) =
 
   let forms = map formAsteroid asteroids
-      formsPlus = (asText game |> toForm) :: forms
 
-  in collage w h formsPlus
+  in collage w h forms
       |> color gray
       |> container w h middle
 
@@ -57,14 +58,24 @@ render (w, h) ({state, asteroids} as game) =
 -- Game
 defaultGame : Game
 defaultGame = {state = Play,
-               asteroids = []}
+               asteroids = [fst <| newAsteroid 16 16 <| Rand.newSeed 0]}
 
 stepGame: Input -> Game -> Game
-stepGame input game = game
+stepGame input game =
+  let _ = Debug.watch "game" game
+      _ = Debug.watch "input" input
+      asteroids = map (stepAsteroid input.dt) game.asteroids
+  in {game | asteroids <- asteroids}
 
 gameState = foldp stepGame defaultGame inputAll
 
 
+
+-- Inputs
+delta = inSeconds <~ fps 30
+inputAll = sampleOn delta (Input <~ delta)
+
+
 main =
   --asText <| circlePoints [0.25, 0.5, 0.75] 1
-  lift2 render Window.dimensions gameState
+  render <~ Window.dimensions ~ gameState
