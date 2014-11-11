a6def496925d87a83122d844e17a19333e500bb8
aver-d <aver-d@someserver.com>
2014-11-11 17:22:34 +0000

    various

--- Asteroids.elm
+++ Asteroids.elm
@@ -4,16 +4,27 @@ import Vec2d as V
 import Window
 import Rand
 import Debug
+import Keyboard
 import Util (bigrams, roundTo)
+
 
 fl = toFloat
 
-type Input = { dt:Time }
+type Input = { dt:Time, thrust: Bool, turn: Float }
+
 
 type Asteroid = {pos: Vec, vel: Vec, accel: Vec, radius: Float, points: [Vec]}
-type Spaceship = {pos: Vec, vel: Vec, accel: Vec, radius: Float, heading: Float}
+type Ship = { pos: Vec
+            , vel: Vec
+            , accel: Vec
+            , damping: Float
+            , maxspeed: Float
+            , turnspeed: Float
+            , radius: Float
+            , heading: Float
+            , thrust: Bool }
 type Game = { state: State
-            , spaceship: Spaceship
+            , ship: Ship
             , asteroids: [Asteroid]
             , seed: Rand.Seed }
 data State = Play
@@ -31,7 +42,7 @@ screenWrapper (w, h) ({pos, radius} as entity) =
   in
     { entity | pos <- (newX, newY) }
 
-wrap = screenWrapper (fl halfW , fl halfH)
+wrap = screenWrapper (fl halfW, fl halfH)
 
 
 
@@ -52,16 +63,45 @@ newAsteroid x y seed =
   in
     ({pos = (x,y), vel = (fl vx, fl vy), accel=(0,0), radius = radius, points = points}, seed5)
 
+newShip : Ship
+newShip =
+  { pos = (0,0), vel = (0,0), accel = (0,0), damping = 0.98, maxspeed = 10
+  , turnspeed = 2, radius = 20, heading = 0, thrust = False }
 
-newSpaceship =
-  {pos = (0,0), vel = (0,0), accel = (0,0), radius = 8, heading = 0}
+movePos dt ent =
+  wrap { ent | pos <- ent.pos `V.add` (dt `V.mul` ent.vel) }
 
---moveEntity : Float -> a -> a
-moveEntity dt ent =
-  let vel = V.add ent.vel <| V.mul ent.accel dt
-      pos = V.add ent.pos <| V.mul vel dt
-  in  wrap { ent | pos <- pos
-                 , vel <- vel }
+
+stepShip {dt, turn, thrust} ship  =
+  ship shipThrust thrust |> shipTurn dt turn |> shipMove dt
+
+
+shipMove dt ship =
+  let vel = ship.vel `V.add` (dt `V.mul` ship.accel)
+              |> V.mul ship.damping
+              |> V.limit ship.maxspeed
+      pos = ship.pos `V.add` (dt `V.mul` vel)
+
+  in  wrap { ship | pos <- pos
+                  , vel <- vel
+                  , accel <- (0,0) }
+
+shipTurn dt turn ship =
+  let heading = if | turn == 0 -> ship.heading
+                   | otherwise -> ship.heading + (turn * ship.turnspeed * dt)
+  in
+    { ship | heading <- heading }
+
+
+shipThrust thrust ship =
+  if | thrust ->
+        let force = (cos ship.heading, sin ship.heading) |> V.mul ship.thrustPower
+        in {ship | accel <- force }
+     | otherwise -> ship
+
+--shipFire
+
+
 
 
 -- Render
@@ -70,18 +110,25 @@ formAsteroid {pos, points} =
   polygon points |> (outlined <| solid black) |> move pos
 
 
-formSpaceship {pos, radius, heading} =
+formShip {pos, radius, heading, thrust} =
   let (x,y) = pos
       r = radius
       p = path [(-r/2,r), (r,0), (-r/2,-r)]
+      body = path p |> (traced (solid darkBrown))
+      booster y = rect (r/2) (r/3) |> filled red |> move (-r, y/4)
+      forms = booster r :: booster -r :: body :: [] |> group
   in
-    path p |> (traced (solid black)) |> move pos |> rotate heading
+    forms |> move pos |> rotate heading
+
+
 
 
 render : (Int,Int) -> Game -> Element
 render (w, h) game =
 
-  let forms = formSpaceship game.spaceship :: (map formAsteroid game.asteroids)
+  let ship = formShip game.ship
+      asteroids = map formAsteroid game.asteroids
+      forms = ship :: asteroids
 
   in collage spaceWidth spaceHeight forms
       |> color gray
@@ -92,7 +139,7 @@ render (w, h) game =
 -- Game
 defaultGame : Game
 defaultGame = { state = Play
-              , spaceship = newSpaceship
+              , ship = newShip
               , asteroids = []
               , seed = Rand.newSeed 0 }
 
@@ -116,21 +163,25 @@ newGame numAsteroids =
 stepGame : Input -> Game -> Game
 stepGame input game =
   let
-      -- _ = Debug.watch "game" game
-      asteroids = map (moveEntity input.dt) game.asteroids
+       --_ = if not <| turn == 0 then let _ = Debug.log "turn" turn in 1 else 1
+      ship = stepShip input game.ship
+      asteroids = map (movePos input.dt) game.asteroids
 
-  in {game | asteroids <- asteroids}
-
+  in {game | asteroids <- asteroids
+           , ship <- ship }
 
 
 
 -- Inputs
 delta = inSeconds <~ fps 30
-inputAll = sampleOn delta (Input <~ delta)
+inputAll = sampleOn delta (Input <~ delta
+                                  ~ lift (.y >> (==) 1 ) Keyboard.arrows  --thrust
+                                  ~ lift (.x >> negate >> toFloat) Keyboard.arrows) --turn
 
 
-gameState = foldp stepGame (newGame 3) inputAll
+gameState = foldp stepGame (newGame 10) inputAll
+
 
 main =
-  --asText <|
+
   render <~ Window.dimensions ~ gameState
