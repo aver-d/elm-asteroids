ad4a0e79617b16ce32490a7013365550d1c266f7
aver-d <aver-d@someserver.com>
2014-11-11 12:37:09 +0000

    filling gaps

--- Asteroids.elm
+++ Asteroids.elm
@@ -4,6 +4,7 @@ import Vec2d as V
 import Window
 import Rand
 import Debug
+import Util (bigrams, roundTo)
 
 fl = toFloat
 
@@ -16,6 +17,9 @@ data State = Play
 
 (spaceWidth, spaceHeight) = (600, 300)
 (halfW, halfH) = (spaceWidth // 2, spaceHeight // 2)
+(quartW, quartH) = (spaceWidth // 4, spaceHeight // 4)
+
+
 
 
 screenWrapper (w, h) ({pos, radius} as entity) =
@@ -28,18 +32,32 @@ screenWrapper (w, h) ({pos, radius} as entity) =
 wrap = screenWrapper (fl halfW / 2, fl halfH / 2)
 
 
--- Entities
+fillGaps maxGap (x::xs) =
+  x :: (concatMap (\(a, b) ->
+         let diff = b - a
+         in if | diff <= maxGap -> [b]
+               | otherwise ->
+                 let n = ceiling (diff/maxGap) |> toFloat
+                     multi = diff / n
+                 in map (\i -> a + i * multi) [1..n]) <| bigrams <| x::xs)
 
+
+
+-- Entities
 newAsteroid : Float -> Float -> Rand.Seed -> (Asteroid, Rand.Seed)
 newAsteroid x y seed =
   let
     ((vx::vy::_), seed2) = Rand.ints -10 10 2 seed
     (r, seed3)           = Rand.int 10 20 seed2
     radius = fl r
-    (npoints, seed4)     = Rand.int 5 8 seed3
+    (npoints, seed4)     = Rand.int 5 10 seed3
     (nums, seed5)        = Rand.floats npoints seed4
 
-    angles = map (\n -> n * 2*pi) nums |> sort
+    --noGaps = fillGaps -1 <| sort nums
+    noGaps = map (roundTo 2) (sort nums)
+    _ = Debug.log "nums" noGaps
+
+    angles = map (\n -> n * 2 *pi) noGaps
     points = map (\a -> (cos a*radius, sin a*radius)) angles
   in
     ({pos = (x,y), vel = (fl vx, fl vy), radius = radius, points = points}, seed5)
@@ -51,11 +69,11 @@ moveEntity dt ent =
   wrap { ent | pos <- V.add ent.pos (V.mul ent.vel dt)}
 
 
-
-
 -- Render
 formAsteroid : Asteroid -> Form
-formAsteroid {pos, radius} = circle radius |> filled black |> move pos
+formAsteroid {pos, points} =
+  polygon points |> (outlined <| solid black) |> move pos
+
 
 
 render : (Int,Int) -> Game -> Element
@@ -65,8 +83,7 @@ render (w, h) ({state, asteroids} as game) =
 
   in collage halfW halfH forms
       |> color gray
-      |> container w h middle
-      -- |> color charcoal
+      |> container w h topLeft
 
 
 
@@ -78,8 +95,8 @@ defaultGame = { state = Play
 
 initGame numAsteroids =
   let seed = Rand.newSeed 0
-      (xs, seed2) = Rand.ints -100 100 numAsteroids seed -- change these to actual window size
-      (ys, seed3) = Rand.ints  -50  50 numAsteroids seed2
+      (xs, seed2) = Rand.ints -quartW quartW numAsteroids seed
+      (ys, seed3) = Rand.ints -quartH quartH numAsteroids seed2
 
       (asteroids, seed4) =
         foldl (\ (x, y) (list, seed) ->
@@ -93,15 +110,14 @@ initGame numAsteroids =
 
 
 
-
-stepGame: Input -> Game -> Game
+stepGame : Input -> Game -> Game
 stepGame input game =
-  let _ = Debug.watch "game" game
-
+  let
+      -- _ = Debug.watch "game" game
       asteroids = map (moveEntity input.dt) game.asteroids
+
   in {game | asteroids <- asteroids}
 
-gameState = foldp stepGame (initGame 5) inputAll
 
 
 
@@ -110,6 +126,8 @@ delta = inSeconds <~ fps 30
 inputAll = sampleOn delta (Input <~ delta)
 
 
+gameState = foldp stepGame (initGame 3) inputAll
+
 main =
-  --asText <| circlePoints [0.25, 0.5, 0.75] 1
+  --asText <|
   render <~ Window.dimensions ~ gameState
